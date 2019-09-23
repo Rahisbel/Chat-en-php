31ee0b699edb57ab22432840cf81392de6c54f37
Rahisbel Herrera <ryhn236@gmail.com>
2019-09-23 16:06:44 -0400

    arreglo en formulario

--- conexionBD.php
+++ conexionBD.php
@@ -10,10 +10,11 @@ class Conexion{
     }
 
     public function EstablecerConexion(){
-        $this->conexion=new mysqli("localhost","root","","Chat");
+        $this->conexion=new mysqli("localhost","root","root","Chat");
+        echo "Conexión establecida  ";
         
         if($this->conexion->connect_errno){
-            echo "Error al conectarse con la Base de Datos";
+            echo "Error al conectarse con la Base de Datos  ";
             exit;
         }                       
     }
